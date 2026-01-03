e388926917ec391a3a5ad1d768adeb688deb641d
zakie <realzzaekkii@gmail.com>
2026-01-04 00:22:58 +0900

    feat: 전체 요일 생성

--- src/main/java/oncall/Week.java
+++ src/main/java/oncall/Week.java
@@ -1,4 +1,17 @@
 package oncall;
 
 public enum Week {
+    MON(false),
+    TUE(false),
+    WED(false),
+    THU(false),
+    FRI(false),
+    SAT(true),
+    SUN(true);
+
+    private final boolean isWeekend;
+
+    Week(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
 }
