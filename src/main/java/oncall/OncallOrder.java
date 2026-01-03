3b5815bd12e2fef97e240c33310e0d025f1e1f88
zakie <realzzaekkii@gmail.com>
2026-01-04 00:50:02 +0900

    feat: 비상 근무 순번 일급 컬렉션 생성

--- src/main/java/oncall/OncallOrder.java
+++ src/main/java/oncall/OncallOrder.java
@@ -1,4 +1,42 @@
 package oncall;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
+
 public class OncallOrder {
+    private Deque<Employee> orders;
+    private Stack<Employee> changeLog;
+
+    public void InitializeOrder(List<Employee> orders) {
+        this.orders = new ArrayDeque<>(orders);
+    }
+
+    public void canWork() {
+        if (changeLog.isEmpty()) {
+            orders.addLast(orders.removeFirst());
+            return;
+        }
+
+        while (!changeLog.isEmpty()) {
+            orders.addLast(changeLog.pop());
+        }
+    }
+
+    public void isContinuous() {
+        Employee continuousEmployee = orders.removeFirst();
+        Employee nextEmployee = orders.removeFirst();
+
+        changeLog.push(nextEmployee);
+        changeLog.push(continuousEmployee);
+    }
+
+    public Employee foundNextEmployee() {
+        if (changeLog.isEmpty()) {
+            return orders.getFirst();
+        }
+
+        return changeLog.peek();
+    }
 }
