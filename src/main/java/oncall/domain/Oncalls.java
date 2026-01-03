30e8858bfe8bf7fcdb9f5d3048a1a9329001afaf
zakie <realzzaekkii@gmail.com>
2026-01-04 01:04:33 +0900

    feat: oncalls 생성 (책임이 좀 헷갈림)

--- src/main/java/oncall/domain/Oncalls.java
+++ src/main/java/oncall/domain/Oncalls.java
@@ -1,4 +1,26 @@
 package oncall.domain;
 
+import oncall.day.Month;
+import oncall.day.Week;
+
+import java.util.List;
+
 public class Oncalls {
+
+    private final Month month;
+    private final Week firstDay;
+    private final OncallOrder weekdayOncallOrder;
+    private final OncallOrder holidayOncallOrder;
+    private List<Oncall> schedule;
+
+    public Oncalls(Month month, Week firstDay, OncallOrder weekdayOncallOrder, OncallOrder holidayOncallOrder) {
+        this.month = month;
+        this.firstDay = firstDay;
+        this.weekdayOncallOrder = weekdayOncallOrder;
+        this.holidayOncallOrder = holidayOncallOrder;
+    }
+
+    public List<Oncall> makeSchedule() {
+
+    }
 }
