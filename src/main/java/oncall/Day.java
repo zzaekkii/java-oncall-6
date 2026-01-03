6e8ced707b670c24c4cc2fdabf38ff253fe56dd2
zakie <realzzaekkii@gmail.com>
2026-01-04 00:31:34 +0900

    feat: 공휴일 및 Day 생성

--- src/main/java/oncall/Day.java
+++ src/main/java/oncall/Day.java
@@ -1,4 +1,16 @@
 package oncall;
 
 public class Day {
+
+    private final Month month;
+    private final int day;
+    private final Week week;
+    private final boolean isHoliday;
+
+    public Day(Month month, int day, Week week, boolean isHoliday) {
+        this.month = month;
+        this.day = day;
+        this.week = week;
+        this.isHoliday = isHoliday;
+    }
 }
---
+++ src/main/java/oncall/Holiday.java
@@ -0,0 +1,22 @@
+package oncall;
+
+public enum Holiday {
+    SINJEONG(1, 1, "신정"),
+    SAMIL(3, 1, "삼일절"),
+    CHILD(5, 5, "어린이날"),
+    MEMORIAL(6, 6, "현충일"),
+    LIBERATION(8, 15, "광복절"),
+    FOUNDATIONAL(10, 3, "개천절"),
+    KOREAN(10, 9, "한글날"),
+    CHRISTMAS(12, 25, "성탄절");
+
+    private final int month;
+    private final int day;
+    private final String label;
+
+    Holiday(int month, int day, String label) {
+        this.month = month;
+        this.day = day;
+        this.label = label;
+    }
+}
